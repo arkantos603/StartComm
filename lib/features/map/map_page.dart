a464374bc5a540604ff639dc80304af07e01526f
João Carlos <joaoarcantos@outlook.com>
2025-01-20 15:40:59 -0300

    map_page

--- lib/features/map/map_page.dart
+++ lib/features/map/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -7,17 +8,61 @@ class MapPage extends StatefulWidget {
   State<MapPage> createState() => _MapPageState();
 }
 
-class _MapPageState extends State<MapPage> 
-    with AutomaticKeepAliveClientMixin<MapPage> {
+class _MapPageState extends State<MapPage> {
+  late GoogleMapController mapController;
+  final LatLng _initialPosition = LatLng(-5.0888351, -42.811246);
+  final Set<Marker> _markers = {};
+
   @override
-  bool get wantKeepAlive => true;
+  void initState() {
+    super.initState();
+    _markers.add(
+      Marker(
+        markerId: MarkerId('initialPosition'),
+        position: _initialPosition,
+        infoWindow: InfoWindow(
+          title: 'IFPI',
+          snippet: 'Instituto Federal do Piauí.',
+        ),
+      ),
+    );
+    _markers.add(
+      Marker(
+        markerId: MarkerId('CASA'),
+        position: LatLng(-5.1119271, -42.77068),
+        infoWindow: InfoWindow(
+          title: 'casita',
+          snippet: 'xd.',
+        ),
+      ),
+    );
+    _markers.add(
+      Marker(
+        markerId: MarkerId('thiagim'),
+        position: LatLng(-4.5791836, -42.8590046),
+        infoWindow: InfoWindow(
+          title: 'INFERNO DA PESTE',
+          snippet: 'casa do goat.',
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    super.build(context);
-    return const Scaffold(
-      body: Center(
-        child: Text('Mapa'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Google Maps Example'),
+      ),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          mapController = controller;
+        },
+        initialCameraPosition: CameraPosition(
+          target: _initialPosition,
+          zoom: 18.0,
+        ),
+        markers: _markers,
       ),
     );
   }
